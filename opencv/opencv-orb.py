64340d9032a2b2c71cff4f26bdee327896962c63
Florian Lier <flier@techfak.uni-bielefeld.de>
2016-04-06 00:14:27 +0200

    godd matches

--- opencv/opencv-orb.py
+++ opencv/opencv-orb.py
@@ -4,11 +4,10 @@ import time
 import sys
 
 cap = cv2.VideoCapture(0)
-
-#sample = cv2.imread('/tmp/card1/card1/positive/card10.png',0)
-sample = cv2.imread('/tmp/crop.png',0)
+sample = cv2.imread('/tmp/foo2.png', 0)
 orb_sample = cv2.ORB()
 kp_sample, des_sample = orb_sample.detectAndCompute(sample, None)
+
 
 def drawMatches(img1, kp1, img2, kp2, matches):
 
@@ -19,13 +18,13 @@ def drawMatches(img1, kp1, img2, kp2, matches):
     rows2 = img2.shape[0]
     cols2 = img2.shape[1]
 
-    out = np.zeros((max([rows1,rows2]),cols1+cols2,3), dtype='uint8')
+    out = np.zeros((max([rows1, rows2]), cols1+cols2, 3), dtype='uint8')
 
     # Place the first image to the left
-    out[:rows1,:cols1] = np.dstack([img1, img1, img1])
+    out[:rows1, :cols1] = np.dstack([img1, img1, img1])
 
     # Place the next image to the right of it
-    out[:rows2,cols1:] = np.dstack([img2, img2, img2])
+    out[:rows2, cols1:] = np.dstack([img2, img2, img2])
 
     # For each pair of points we have between both images
     # draw circles, then connect a line between them
@@ -40,66 +39,71 @@ def drawMatches(img1, kp1, img2, kp2, matches):
 
         # x - columns
         # y - rows
-        (x1,y1) = kp1[img1_idx].pt
-        (x2,y2) = kp2[img2_idx].pt
+        (x1, y1) = kp1[img1_idx].pt
+        (x2, y2) = kp2[img2_idx].pt
 
-	x += kp1[img1_idx].pt[0]
+        x += kp1[img1_idx].pt[0]
         y += kp1[img1_idx].pt[1]
 
-        # Draw a small circle at both co-ordinates
-        # radius 4
-        # colour blue
-        # thickness = 1
-        cv2.circle(out, (int(x1),int(y1)), 4, (255, 0, 0), 1)   
-        cv2.circle(out, (int(x2)+cols1,int(y2)), 4, (255, 0, 0), 1)
+    # Draw a small circle at both co-ordinates
+    # radius 4
+    # colour blue
+    # thickness = 1
+    cv2.circle(out, (int(x1), int(y1)), 4, (255, 0, 0), 1)
+    cv2.circle(out, (int(x2)+cols1, int(y2)), 4, (255, 0, 0), 1)
 
-        # Draw a line in between the two points
-        # thickness = 1
-        # colour blue
-        cv2.line(out, (int(x1),int(y1)), (int(x2)+cols1,int(y2)), (255, 0, 0), 1)
+    # Draw a line in between the two points
+    # thickness = 1
+    # colour blue
+    cv2.line(out, (int(x1), int(y1)), (int(x2)+cols1, int(y2)), (255, 0, 0), 1)
+    # cv2.circle(out, (int(x/10), int(y/10)), 4, (255, 255, 0), 1)
 
-    cv2.circle(out, (int(x/10),int(y/10)), 4, (255, 255, 0), 1)
     return out
 
 while True:
 
-	ret, frame = cap.read()
+    ret, frame = cap.read()
 
-	if ret is not True:
-		continue
-	img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    if ret is not True:
+        continue
 
-	try:
-        	# Initiate STAR detector
-		orb_obs = cv2.ORB()
-	
-        	# Find the keypoints with ORB
-		kp_obs = orb_obs.detect(img, None)
-	
-        	# Compute the descriptors with ORB
-		kp_obs, des_obs = orb_obs.detectAndCompute(img, None)
+    img = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-		# Create BFMatcher object
-		bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+    try:
+        # Initiate STAR detector
+        orb_obs = cv2.ORB()
 
-		# Match descriptors.
-		matches = bf.match(des_sample, des_obs)
-	except Exception, e:
-		pass
+        # Find the keypoints with ORB
+        kp_obs = orb_obs.detect(img, None)
 
-	# Sort them in the order of their distance.
-	try:
-		matches = sorted(matches, key = lambda x:x.distance)
-		print matches[1].distance
-	except Exception, e:
-		pass
+        # Compute the descriptors with ORB
+        kp_obs, des_obs = orb_obs.detectAndCompute(img, None)
 
-	# Draw first 10 matches.
-	try:
-		result = drawMatches(img, kp_obs, sample, kp_sample, matches[:10])
-	        cv2.imshow(':: ORBPy (Quit: press q) ::', result)
-	except Exception, e:
-		pass
+        # Create BFMatcher object
+        bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
 
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            sys.exit(1)
+        # Match descriptors.
+        matches = bf.match(des_sample, des_obs)
+
+    except Exception, e:
+        print e
+
+    # Sort them in the order of their distance.
+    try:
+        matches = sorted(matches, key = lambda x:x.distance)
+        print matches[1].distance
+    except Exception, e:
+        print "MATCHES: ", e
+
+    # Draw first 10 matches.
+    try:
+        if len(matches) < 1:
+            print "No Matches"
+            continue
+        result = drawMatches(img, kp_obs, sample, kp_sample, matches[:1])
+        cv2.imshow(':: ORBPy (Quit: press q) ::', result)
+    except Exception, e:
+        print "IMSHOW ", e
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        sys.exit(1)
