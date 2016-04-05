7de0dee49402300cb4c82c42c620996f86fdb7df
Florian Lier <flier@techfak.uni-bielefeld.de>
2016-04-05 23:13:56 +0200

    first example working

--- opencv/src/opencv-surf.cpp
+++ opencv/src/opencv-surf.cpp
@@ -1,61 +1,134 @@
+/*
+ * @file SURF_FlannMatcher
+ * @brief SURF detector + descriptor + FLANN Matcher
+ * @author A. Huaman
+ */
+#include <stdio.h>
+#include <iostream>
 #include <stdio.h>
 #include <iostream>
 #include "opencv2/core.hpp"
+#include "opencv2/opencv.hpp"
 #include "opencv2/features2d.hpp"
-#include "opencv2/xfeatures2d.hpp"
+#include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
-
+#include "opencv2/xfeatures2d.hpp"
+using namespace std;
 using namespace cv;
 using namespace cv::xfeatures2d;
-
 void readme();
-
-/** @function main */
+/*
+ * @function main
+ * @brief Main function
+ */
 int main( int argc, char** argv )
 {
-  if( argc != 3 )
-  { readme(); return -1; }
+
+  if( argc != 2 ) {
+    readme(); return -1;
+  }
+
+  VideoCapture cap(0); // open the default camera
+
+  if(!cap.isOpened()) {  // check if we succeeded
+    return -1;
+  }
+
+  namedWindow(":: OTC FLANN SURF MATCHES ::", 1);
 
   Mat img_1 = imread( argv[1], IMREAD_GRAYSCALE );
-
-  if( !img_1.data || !img_2.data )
-  { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
-
-  //-- Step 1: Detect the keypoints using SURF Detector
-  int minHessian = 400;
-
-  Ptr<SURF> detector = SURF::create( minHessian );
-
-  std::vector<KeyPoint> keypoints_1, keypoints_2;
-  detector->detect( img_1, keypoints_1 );
-
-  VideoCapture cap(0);
-  if(!cap.isOpened())
-    return -1;
-
+  Mat img_2;
   Mat frame;
-  namedWindow(":: OTC SURF ::",1);
 
-  for(;;)
-  {
-    Mat frame;
-    cap >> frame; // get a new frame from camera
-    cvtColor(frame, COLOR_BGR2GRAY);
+  //-- Step 1: Detect the keypoints using SURF Detector, compute the descriptors
+  int minHessian = 400;
+  Ptr<SURF> detector = SURF::create();
+  detector->setHessianThreshold(minHessian);
+  std::vector<KeyPoint> keypoints_1, keypoints_2;
+  Mat descriptors_1, descriptors_2;
+  detector->detectAndCompute( img_1, Mat(), keypoints_1, descriptors_1 );
+  FlannBasedMatcher matcher;
+  std::vector<DMatch> matches;
+  std::vector<DMatch> good_matches;
 
-    //-- Draw keypoints
-    Mat img_keypoints_1;
-    drawKeypoints(frame, keypoints_1, img_keypoints_1, Scalar::all(-1), DrawMatchesFlags::DEFAULT );
+  for(;;) {
 
-    //-- Show detected (drawn) keypoints
-    imshow("Keypoints 1", img_keypoints_1 );
+    cap >> frame;
 
-    if(waitKey(30) >= 0) break;
+    // if (frame.rows < img_1.rows) {
+    //     continue;
+    // }
+
+    cvtColor(frame, img_2, COLOR_BGR2GRAY);
+
+    if( !img_1.data || !img_2.data ) {
+        std::cout<< " --(!) Error reading images " << std::endl;
+        return -1;
+    }
+
+    detector->detectAndCompute(img_2, Mat(), keypoints_2, descriptors_2);
+
+    //-- Step 2: Matching descriptor vectors using FLANN matcher
+    matcher.match(descriptors_1, descriptors_2, matches);
+
+    double max_dist = 0; double min_dist = 100;
+
+    //-- Quick calculation of max and min distances between keypoints
+    for( int i = 0; i < descriptors_1.rows; i++ ) {
+        double dist = matches[i].distance;
+        if( dist < min_dist ) {
+            min_dist = dist;
+        }
+
+        if( dist > max_dist ) {
+            max_dist = dist;
+        }
+    }
+
+    printf("-- Max dist : %f \n", max_dist );
+    printf("-- Min dist : %f \n", min_dist );
+
+    //-- Draw only "good" matches (i.e. whose distance is less than 2*min_dist,
+    //-- or a small arbitary value ( 0.02 ) in the event that min_dist is very
+    //-- small)
+    //-- PS.- radiusMatch can also be used here.
+
+    for( int i = 0; i < descriptors_1.rows; i++ ) {
+        if(matches[i].distance <= max(2*min_dist, 0.02) ) {
+            good_matches.push_back( matches[i]);
+        }
+    }
+
+    //-- Draw only "good" matches
+    Mat img_matches;
+    try {
+        drawMatches( img_1, keypoints_1, img_2, keypoints_2,
+                    good_matches, img_matches, Scalar::all(-1), Scalar::all(-1),
+                    vector<char>(), DrawMatchesFlags::NOT_DRAW_SINGLE_POINTS );
+    } catch(...) {
+        continue;
+    }
+
+    //-- Show detected matches
+    imshow( ":: OTC FLANN SURF MATCHES ::", img_matches );
+
+    /* for( int i = 0; i < (int)good_matches.size(); i++ ) {
+        printf( "-- Good Match [%d] Keypoint 1: %d  -- Keypoint 2: %d  \n", i, good_matches[i].queryIdx, good_matches[i].trainIdx );
+    } */
+
+    if(waitKey(30) >= 0) {
+        break;
+    }
+
   }
 
   return 0;
 
-  }
+}
 
-  /** @function readme */
-  void readme()
-  { std::cout << " Usage: ./SURF_detector <img1> <img2>" << std::endl; }
+/*
+ * @function readme
+ */
+void readme() {
+    std::cout << " Usage: ./otc-opencv-surf <img1>" << std::endl;
+}
