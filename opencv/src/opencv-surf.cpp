b85005603e6c2fd7d600fc4688e5eac07a88da0c
Florian Lier <flier@techfak.uni-bielefeld.de>
2016-04-05 16:44:41 +0200

    opencv3 version of surf

--- opencv/src/opencv-surf.cpp
+++ opencv/src/opencv-surf.cpp
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <iostream>
-#include "opencv2/core/core.hpp"
-#include "opencv2/features2d/features2d.hpp"
-#include "opencv2/nonfree/features2d.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include "opencv2/nonfree/nonfree.hpp"
+#include "opencv2/core.hpp"
+#include "opencv2/features2d.hpp"
+#include "opencv2/xfeatures2d.hpp"
+#include "opencv2/highgui.hpp"
 
 using namespace cv;
+using namespace cv::xfeatures2d;
 
 void readme();
 
@@ -16,8 +16,8 @@ int main( int argc, char** argv )
   if( argc != 3 )
   { readme(); return -1; }
 
-  Mat img_1 = imread( argv[1], CV_LOAD_IMAGE_GRAYSCALE );
-  Mat img_2 = imread( argv[2], CV_LOAD_IMAGE_GRAYSCALE );
+  Mat img_1 = imread( argv[1], IMREAD_GRAYSCALE );
+  Mat img_2 = imread( argv[2], IMREAD_GRAYSCALE );
 
   if( !img_1.data || !img_2.data )
   { std::cout<< " --(!) Error reading images " << std::endl; return -1; }
@@ -25,12 +25,12 @@ int main( int argc, char** argv )
   //-- Step 1: Detect the keypoints using SURF Detector
   int minHessian = 400;
 
-  SurfFeatureDetector detector( minHessian );
+  Ptr<SURF> detector = SURF::create( minHessian );
 
   std::vector<KeyPoint> keypoints_1, keypoints_2;
 
-  detector.detect( img_1, keypoints_1 );
-  detector.detect( img_2, keypoints_2 );
+  detector->detect( img_1, keypoints_1 );
+  detector->detect( img_2, keypoints_2 );
 
   //-- Draw keypoints
   Mat img_keypoints_1; Mat img_keypoints_2;
